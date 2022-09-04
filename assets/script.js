4c242451966fd9c2d077c8627b7be5ba176e2273
Chibuike Okoye <talk2chibyke@yahoo.com>
2022-09-04 23:12:24 +1000

    created function to color code the timeblocks relative to the current time and also retrive event from local storage to display on corresponding text block

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,40 @@ $(document).ready(function () {
         var time = $(this).parent().attr('id');
         localStorage.setItem(time, JSON.stringify(description));
     });
+    // displays color code for timeblock relative to the current time (past, present and future)
+    function currentTime() {
+        var current = moment().hours()
+        var timeBlock = $('.time-block')
+        timeBlock.each(function () {
+            var hour = parseInt($(this).attr("id"))
+            // color code if time is at the present
+            if (hour === current) {
+                $(this).children('.col-sm-10').attr('class', 'present col-sm-10')
+            }
+            // color code if time has past
+            else if (current > hour) {
+                $(this).children('.col-sm-10').attr('class', 'past col-sm-10')
+            }
+            // color code if time is in the future
+            else {
+                $(this).children('.col-sm-10').attr('class', 'future col-sm-10')
+            }
+        })
+    };
+    currentTime()
+    // retrieves schedule from the local storage and displays on the corresponding textblock
+    $("#5 textarea").val(JSON.parse(localStorage.getItem("5")))
+    $("#6 textarea").val(JSON.parse(localStorage.getItem("6")))
+    $("#7 textarea").val(JSON.parse(localStorage.getItem("7")))
+    $("#8 textarea").val(JSON.parse(localStorage.getItem("8")))
+    $("#9 textarea").val(JSON.parse(localStorage.getItem("9")))
+    $("#10 textarea").val(JSON.parse(localStorage.getItem("10")))
+    $("#11 textarea").val(JSON.parse(localStorage.getItem("11")))
+    $("#12 textarea").val(JSON.parse(localStorage.getItem("12")))
+    $("#13 textarea").val(JSON.parse(localStorage.getItem("13")))
+    $("#14 textarea").val(JSON.parse(localStorage.getItem("14")))
+    $("#15 textarea").val(JSON.parse(localStorage.getItem("15")))
+    $("#16 textarea").val(JSON.parse(localStorage.getItem("16")))
+    $("#17 textarea").val(JSON.parse(localStorage.getItem("17")))
+    $("#18 textarea").val(JSON.parse(localStorage.getItem("18")))
+})
